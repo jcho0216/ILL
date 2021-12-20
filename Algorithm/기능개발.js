901905e8bdda05d79892db43b8296e820ff03a11
Jun Joy <chojuneseo1@gmail.com>
2021-12-20 10:27:51 +0900

    프로그래머스: 기능개발

--- Algorithm/기능개발.js
+++ Algorithm/기능개발.js
@@ -1,45 +1,45 @@
 //나의 풀이:
 function solution(progresses, speeds) {
-    const days = [];
-    const answer = [];
-    let time = 1;
+  let days = [];
+  const answer = [];
+  let time = 1;
 
-    for(let i = 0; i < progresses.length; i++) {
-        days.push(Math.ceil((100 - progresses[i]) / speeds[i]));
+  for (let i = 0; i < progresses.length; i++) {
+    days.push(Math.ceil((100 - progresses[i]) / speeds[i]));
+  }
+
+  for (let i = 0; i < days.length; i++) {
+    if (days[i] >= days[i + 1]) {
+      days[i + 1] = days[i];
     }
 
-    for(let i = 0; i < days.length; i++) {
-        if(days[i] >= days[i + 1]) {
-            days[i + 1] = days[i];
-        }
+    if (days[i] === days[i + 1]) {
+      time++;
+    } else {
+      answer.push(time);
+      time = 1;
     }
+  }
 
-    for(let i = 0; i < days.length; i++) {       
-        if(days[i] === days[i + 1]) {        
-            time++;
-        } else {
-            answer.push(time);           
-            time = 1;
-        }
-    }
-
-    return answer;
+  return answer;
 }
 
 //남의 풀이: 나보다 훨씬 간단하게 잘 짰구나..
 function solution(progresses, speeds) {
-    let answer = [0];
-    let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
-    let maxDay = days[0];
+  let answer = [0];
+  let days = progresses.map((progress, index) =>
+    Math.ceil((100 - progress) / speeds[index])
+  );
+  let maxDay = days[0];
 
-    for(let i = 0, j = 0; i< days.length; i++){
-        if(days[i] <= maxDay) {
-            answer[j] += 1;
-        } else {
-            maxDay = days[i];
-            answer[++j] = 1;
-        }
+  for (let i = 0, j = 0; i < days.length; i++) {
+    if (days[i] <= maxDay) {
+      answer[j] += 1;
+    } else {
+      maxDay = days[i];
+      answer[++j] = 1;
     }
+  }
 
-    return answer;
+  return answer;
 }
